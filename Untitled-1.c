682fc55ccdb23b90ee5f4cceb51db7cff8f479d9
Abhishek <gargkumar4848@gmail.com>
2023-05-31 23:56:47 +0530

    Testt

---
+++ Untitled-1.c
@@ -0,0 +1,12 @@
+//C Program to print numbers in reverse order
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    for (n=6; n; n--)
+    {
+        printf("Natural numbers are %d \n", n);
+    }
+    return 0;
+}
